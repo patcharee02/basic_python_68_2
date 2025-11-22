88eea2f39d10142de315089292d41c2825630fd2
patcharee02 <Patcharee252124@gmail.com>
2025-11-22 13:26:50 +0700

    Variabal

---
+++ python1.py
@@ -0,0 +1,45 @@
+"""
+# 
+# Part: python comment
+#
+"""
+
+"""
+v = s / t
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print("Hello, World!")
+
+"""
+#
+# Part: Python Variable
+#
+"""
+
+
+
+x = 5
+y = "5"
+print(x,y)
+
+x = str(5)
+y = int(10)
+z = float(15)
+print(type(x), type(y), type(z))
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+MY_VAR = "John"
+my_var2 = "John"
+
+# 2my_var = "John"
+# my-var = "John"
+# my var = "John"
+
+# Camel Case
