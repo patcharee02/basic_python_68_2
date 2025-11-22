23945536295813d9f43d0eb5722363002c05980e
patcharee02 <Patcharee252124@gmail.com>
2025-11-22 14:59:28 +0700

    Loop

---
+++ python5.py
@@ -0,0 +1,22 @@
+"""
+#
+# Part: Loops
+# Section: while Loop
+"""
+i = 1
+while i <= 5:
+    print("hello python", i)
+    if i == 3:
+        break
+    i += 1
+
+"""
+#
+# Part: Loops
+# Section: while Loop
+"""
+this_list = ["apple", "banana", "cherry"]
+for x in this_list:
+    print(x)
+    if x == "banana":
+        break
