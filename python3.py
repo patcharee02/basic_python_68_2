2a3d99dab5a8e7ac2c95fa89439f4706029eea02
patcharee02 <Patcharee252124@gmail.com>
2025-11-22 14:32:31 +0700

    Collection

---
+++ python3.py
@@ -0,0 +1,54 @@
+"""
+#
+# Part: Collection of Python 
+# List, Tuple, Set, Dictionary
+#
+"""
+"""
+#
+# List
+#
+"""
+
+this_list = ["apple", "banana", "cherry", "apple", "cherry"]
+print(this_list)
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# Tuple
+# 
+"""
+this_tuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(this_tuple)
+print(this_tuple[1])
+print(type(this_tuple))
+print(len(this_tuple))
+
+"""
+#
+# Set
+#
+"""
+this_set = {"apple", "banana", "apple","cherry"}
+print(this_set)
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+# Dictionary
+# 
+"""
+this_dict = {
+    "name" : "John",
+    "age" : 36,
+    "country" : "Norway"
+
+}
+print(this_dict)
+print(this_dict["name"])
+print(type(this_dict))
+print(len(this_dict))
